0f33a7834fae5ff69f33ad258dd266300fc58e7f
Lauren West <laurenwest12@gmail.com>
2019-04-25 21:00:33 -0400

    test

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 
 const port = process.env.PORT || 3000;
 
-app.get('/app.js', (req, res, next)=> res.sendFile(path.join(__dirname, 'dist', 'main.js')));
+app.get("/app.js", (req, res, next) =>
+  res.sendFile(path.join(__dirname, "dist", "main.js"))
+);
 
-app.get('/', (req, res, next)=> res.sendFile(path.join(__dirname, 'index.html')));
+app.get("/", (req, res, next) =>
+  res.sendFile(path.join(__dirname, "index.html"))
+);
 
+app.get("*", (req, res, next) => {
+  console.log("hello");
+});
 
-
-app.listen(port, ()=> console.log(`listening on port ${port}`))
+app.listen(port, () => console.log(`listening on port ${port}`));
