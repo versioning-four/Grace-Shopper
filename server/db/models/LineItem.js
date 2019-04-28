ac65c7895b1e25a494404e9a2a1be8a2697315a2
Steven Levine <slevine111@gmail.com>
2019-04-28 16:00:39 -0400

    created lineitem model

---
+++ server/db/models/LineItem.js
@@ -0,0 +1,22 @@
+const db = require('../db')
+const Sequelize = require('sequelize')
+
+const LineItem = db.define('lineitem', {
+  id: {
+    type: Sequelize.UUID,
+    defaultValue: Sequelize.UUIDV4,
+    primaryKey: true
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    defaultValue: 1,
+    validate: {
+      min: 1
+    }
+  },
+  orderid: {
+    type: Sequelize.INTEGER
+  }
+})
+
+module.exports = LineItem
