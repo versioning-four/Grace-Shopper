c7206b6b4c21d326104c1dbba37d641e030ad00b
Steven Levine <slevine111@gmail.com>
2019-04-28 18:36:56 -0400

    made a .then for seeing data of each model

--- server/db/seed.js
+++ server/db/seed.js
@@ -2,12 +2,24 @@ const { Order, LineItem, Review, User, Product } = require('./models')
 const db = require('./db')
 
 const syncAndSeed = () => {
-
-    return db.authenticate()
-    .then(() => db.sync({force: true}))
+  return db
+    .authenticate()
+    .then(() => db.sync({ force: true }))
     .then(() => {
-        //EVERYVODY CREATE SEED DATA
+      //CREATE ORDER DATA
+    })
+    .then(() => {
+      //CREATE LINEITEM DATA
+    })
+    .then(() => {
+      //CREATE REVIEW DATA
+    })
+    .then(() => {
+      //CREATE USER DATA
+    })
 
+    .then(() => {
+      //CREATE PRODUCT DATA
     })
 }
 
