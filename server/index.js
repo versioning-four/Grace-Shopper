9c4816ca9b607fd2ec545b33f392458e8b8cff88
Steven Levine <slevine111@gmail.com>
2019-04-28 17:28:46 -0400

    changed order model, got rid of double quotes, and created api folder

--- server/index.js
+++ server/index.js
@@ -1,17 +1,18 @@
-const express = require("express")
+const express = require('express')
 const app = express()
-const path = require("path")
+const path = require('path')
 const syncAndSeed = require('./seed')
 
 const port = process.env.PORT || 3000
 
-app.get("/app.js", (req, res, next) =>
-  res.sendFile(path.join(__dirname, "dist", "main.js"))
+app.get('/app.js', (req, res, next) =>
+  res.sendFile(path.join(__dirname, 'dist', 'main.js'))
 )
 
-app.get("/", (req, res, next) =>
-  res.sendFile(path.join(__dirname, "index.html"))
+app.get('/', (req, res, next) =>
+  res.sendFile(path.join(__dirname, 'index.html'))
 )
 
-syncAndSeed()
-  .then(() => app.listen(port, () => console.log(`listening on port ${port}`)))
+syncAndSeed().then(() =>
+  app.listen(port, () => console.log(`listening on port ${port}`))
+)
