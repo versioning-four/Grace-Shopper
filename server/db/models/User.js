5724175ea74e951b49fc72a380b667c4b6426100
Juan Torres <jet@Juans-MacBook-Pro.local>
2019-04-28 16:06:35 -0400

    added uuid for id

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -2,6 +2,10 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const User = db.define('user', {
+    id: {
+        type: Sequelize.UUID
+    },
+
     firstName: {
         type: Sequelize.STRING
     },
