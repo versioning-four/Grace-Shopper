1e15ad6f1202b8e413a507c3c73bf62a859d8ea1
Steven Levine <slevine111@gmail.com>
2019-04-30 21:15:30 -0400

    got amazing product page

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { getAllProductsThunk } from '../redux/actions'
 import Products from './Products'
+import SingleProduct from './SingleProduct'
 
 class App extends Component {
   componentDidMount() {
@@ -13,6 +14,7 @@ class App extends Component {
     return (
       <Router>
         <Route exact path="/products" component={Products} />
+        <Route path="/products/:id" component={SingleProduct} />
       </Router>
     )
   }
