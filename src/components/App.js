04ce3c73d4285367db74cefc136964acfca6f19b
samcomma <samcomerford1825@gmail.com>
2019-04-30 20:16:31 -0400

    set up front-end structure

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import {HashRouter as Router, Switch, Route} from 'react-router-dom'
+
+
+
+class App extends Component {
+  render () {
+    return (
+      <Router>
+      </Router>
+    )
+  }
+}
+
+
+export default App
