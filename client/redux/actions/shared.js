188ae8ee478c44a1357459c1e044da0fadffb7b9
samcomma <samcomerford1825@gmail.com>
2019-05-09 21:40:41 -0400

    cart persistence

--- client/redux/actions/shared.js
+++ client/redux/actions/shared.js
@@ -4,11 +4,15 @@ import { setUserOrders } from './userOrders'
 
 export const processAfterHaveUserThunk = (userId, newOrder) => {
   return async dispatch => {
-    let order = await axios.get(`/api/users/${userId}/orders/cart`)
-    if (!order.data) {
-      order = await axios.post(`/api/users/${userId}/orders`, newOrder)
+    if (userId) {
+      let order = await axios.get(`/api/users/${userId}/orders/cart`)
+      if (!order.data) {
+        order = await axios.post(`/api/users/${userId}/orders`, newOrder)
+      }
+      dispatch(setUserOrders([order.data]))
+      return dispatch(getOrderLineitemsThunk(userId, order.data.id))
+    } else {
+      return dispatch(getOrderLineitemsThunk(0, 0))
     }
-    dispatch(setUserOrders([order.data]))
-    return dispatch(getOrderLineitemsThunk(userId, order.data.id))
   }
 }
--- server/api/lineitemsbyuserandorder.js
+++ server/api/lineitemsbyuserandorder.js
@@ -3,19 +3,57 @@ const { LineItem } = require('../db/models/index')
 module.exports = router
 
 router.post('/', (req, res, next) => {
-  LineItem.create(req.body)
-    .then(lineitem => res.json(lineitem))
-    .catch(next)
+  if (req.session.userId) {
+    LineItem.create(req.body)
+      .then(lineitem => res.json(lineitem))
+      .catch(next)
+  } else {
+    if (req.session.cart) {
+      req.session.cart.push(req.body)
+    } else {
+      req.session.cart = [req.body]
+    }
+    console.log(req.session.cart)
+    res.json(req.body)
+  }
 })
 
 router.get('/', (req, res, next) => {
-  LineItem.findAll({
-    where: {
-      orderId: req.orderId
-    }
-  })
-    .then(lineitems => res.json(lineitems))
-    .catch(next)
+  if (req.session.userId) {
+    LineItem.findAll({
+      where: {
+        orderId: req.orderId
+      }
+    })
+      .then(lineitems => {
+        if (req.session.cart && req.session.cart.length) {
+          req.session.cart.forEach(async item => {
+            let existingLineItem = lineitems.find(
+              lineitem => lineitem.productId === item.productId
+            )
+            if (existingLineItem) {
+              await LineItem.update(
+                { quantity: existingLineItem.quantity + item.quantity },
+                { where: { id: existingLineItem.id } }
+              )
+            } else {
+              const newLineItem = await LineItem.create({
+                orderId: req.orderId,
+                productId: item.productId,
+                quantity: item.quantity
+              })
+              lineitems.push(newLineItem)
+            }
+          })
+        }
+        return lineitems
+      })
+      .then(lineitems => res.json(lineitems))
+      .catch(next)
+  } else {
+    req.session.cart = req.session.cart || []
+    res.json(req.session.cart)
+  }
 })
 
 router.put('/:lineitemid', (req, res, next) => {
